17c8420b9b4cbec4646d406ba40fbc2e62e58dcc
Haxel Escobar <haxelesco@gmail.com>
2024-09-10 14:28:01 -0500

    se agrega el proyecto

---
+++ PROYECTO/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import  './components/NavBar/NavBar'
+import './components/CartWidget/CartWidget'
+import './components/ItemListContainer/ItemListContainer'
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
+function App() {
+    return (
+    <>
+    
+      <NavBar/> 
+      <ItemListContainer greeting="Bienvenidos"/>
+    
+    </>
+  )
+}
+
+export default App
